da06529a8c15a6f2e09d3c0982e7c6a734aeb025
Diya-Dangol <diya.dangol15@gmail.com>
2021-01-17 10:57:23 +0545

    Updates

---
+++ precache-manifest.74e910cd2b4ce9b8cae0a45e4fe48ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41643279e2f20b2a7c169fc0f0f0be51",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "2eb353ce009b6976a817",
+    "url": "/weather-app/static/css/main.6c0e74d0.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb353ce009b6976a817",
+    "url": "/weather-app/static/js/main.8d842f29.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
